5c2461a2652b2c2debb387980a3423e0c2d7a61c
Ankit Dabhi <dabhiankit1404@gmail.com>
2024-06-21 14:15:07 +0530

    21-06-02-15

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,9 +1,14 @@
-const port = 4000;
+const port = 3000;
 
 export const env  = {
     port : port,
-    backendUrl:`http://localhost:${port}/api`,
-    socketUrl : `ws://localhost:${port}/api`,
+    backendUrl:`/api`,
+    socketUrl : `/api`,
 }
+// export const env  = {
+//     port : port,
+//     backendUrl:`http://localhost:${port}/api`,
+//     socketUrl : `ws://localhost:${port}/api`,
+// }
 
 // http://16.171.176.28/
